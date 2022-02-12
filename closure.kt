2c848bf3dfc3cf0cb74dce2cc8558e61aad1e11a
Grigoriy Khlytin <xlootin@niuitmo.ru>
2022-02-12 09:13:59 +0300

    update closure.kt

--- closure.kt
+++ closure.kt
@@ -7,7 +7,7 @@ class Attribute(inputName: String) {
         }
     }
 
-    private val name: String = inputName
+    private var name: String = inputName
 
     init {
         if (name == "") {
@@ -48,14 +48,14 @@ class Attribute(inputName: String) {
 }
 
 class CommonRule {
-    private val left: HashSet<Attribute> = HashSet()
-    private val right: HashSet<Attribute> = HashSet()
+    private val left = AttributesPack()
+    private val right = AttributesPack()
 
-    fun getLeft(): HashSet<Attribute> {
+    fun getLeft(): AttributesPack {
         return left
     }
 
-    fun getRight(): HashSet<Attribute> {
+    fun getRight(): AttributesPack {
         return right
     }
 
@@ -87,11 +87,63 @@ class CommonRule {
 
     override fun toString(): String {
         var ans = "CommonRule {"
-        for (x in left) ans += ("$x,")
-        if (left.size != 0) ans = ans.dropLast(1)
+        for (x in left.get()) ans += ("$x,")
+        if (left.get().size != 0) ans = ans.dropLast(1)
         ans += "->"
-        for (x in right) ans += ("$x,")
-        if (right.size != 0) ans = ans.dropLast(1)
+        for (x in right.get()) ans += ("$x,")
+        if (right.get().size != 0) ans = ans.dropLast(1)
+        ans += "}"
+        return ans
+    }
+}
+
+class SimpleRule {
+    private var left = AttributesPack()
+    private var right: Attribute? = null
+
+    fun getLeft(): AttributesPack {
+        return left
+    }
+
+    fun getRight(): Attribute {
+        if (right == null) {
+            throw RuntimeException("unexpected null exception")
+        }
+        return right as Attribute
+    }
+
+    fun addLeft(arg: AttributesPack) {
+        left.add(arg)
+    }
+
+    fun addRight(arg: Attribute) {
+        right = arg
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as SimpleRule
+
+        if (left != other.left) return false
+        if (right != other.right) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = left.hashCode()
+        result = 31 * result + (right?.hashCode() ?: 0)
+        return result
+    }
+
+    override fun toString(): String {
+        var ans = "SimpleRule {"
+        for (x in left.get()) ans += ("$x,")
+        if (left.get().size != 0) ans = ans.dropLast(1)
+        ans += "->"
+        ans += ("$right")
         ans += "}"
         return ans
     }
@@ -118,14 +170,46 @@ class RulesPack {
     }
 }
 
+class SimpleRulesPack {
+    val rules: HashSet<SimpleRule> = HashSet()
+
+    fun addRule(arg: SimpleRule) {
+        rules.add(arg)
+    }
+
+    fun copy(arg: SimpleRulesPack) {
+        rules.addAll(arg.rules)
+    }
+
+    fun removeRule(arg: SimpleRule) {
+        rules.remove(arg)
+    }
+
+    override fun toString(): String {
+        var ans = "SimpleRulesPack {\n"
+        for (x in rules) {
+            ans += ("    $x\n")
+        }
+        ans += "}"
+        return ans
+    }
+}
+
 class AttributesPack {
     private val attributes: HashSet<Attribute> = HashSet()
 
-    fun contains(args: HashSet<Attribute>): Boolean {
-        for (arg in args) {
+    fun containsAll(args: AttributesPack): Boolean {
+        for (arg in args.get()) {
             if (!attributes.contains(arg)) {
                 return false
             }
+        }
+        return true
+    }
+
+    fun contains(arg: Attribute): Boolean {
+        if (!attributes.contains(arg)) {
+            return false
         }
         return true
     }
@@ -138,12 +222,12 @@ class AttributesPack {
         attributes.add(arg)
     }
 
-    fun add(args: HashSet<Attribute>) {
-        attributes.addAll(args)
-    }
-
     fun add(args: AttributesPack) {
         attributes.addAll(args.attributes)
+    }
+
+    fun remove(arg: Attribute) {
+        attributes.remove(arg)
     }
 
     override fun toString(): String {
@@ -161,7 +245,7 @@ fun getClosure(attributesPack: AttributesPack, rulesPack: RulesPack): Attributes
     while (true) {
         val cnt = curr.get().size
         for (rule in rulesPack.getRules()) {
-            if (curr.contains(rule.getLeft())) {
+            if (curr.containsAll(rule.getLeft())) {
                 curr.add(rule.getRight())
             }
         }
@@ -169,6 +253,88 @@ fun getClosure(attributesPack: AttributesPack, rulesPack: RulesPack): Attributes
             break
         }
     }
+    return curr
+}
+
+fun getClosure(attributesPack: AttributesPack, rulesPack: SimpleRulesPack): AttributesPack {
+    val curr = AttributesPack()
+    curr.add(attributesPack)
+    while (true) {
+        val cnt = curr.get().size
+        for (rule in rulesPack.rules) {
+            if (curr.containsAll(rule.getLeft())) {
+                curr.add(rule.getRight())
+            }
+        }
+        if (curr.get().size == cnt) {
+            break
+        }
+    }
+    return curr
+}
+
+fun closureIsLess(firstArg: RulesPack, secondArg: RulesPack): Boolean {
+    for (rule in firstArg.getRules()) {
+        val closure: AttributesPack = getClosure(rule.getLeft(), secondArg)
+        if (!closure.containsAll(rule.getRight())) {
+            return false
+        }
+    }
+    return true
+}
+
+fun closureEquals(firstArg: RulesPack, secondArg: RulesPack): Boolean {
+    return closureIsLess(firstArg, secondArg) && closureIsLess(secondArg, firstArg)
+}
+
+fun getIrreducible(rulesPack: RulesPack): SimpleRulesPack {
+    var curr = SimpleRulesPack()
+    for (rule in rulesPack.getRules()) {
+        for (r in rule.getRight().get()) {
+            val newRule = SimpleRule()
+            newRule.addLeft(rule.getLeft())
+            newRule.addRight(r)
+            curr.addRule(newRule)
+        }
+    }
+
+    val deq: ArrayDeque<SimpleRule> = ArrayDeque()
+    for (x in curr.rules) {
+        deq.add(x)
+    }
+
+    while (!deq.isEmpty()) {
+        val rule = deq.removeFirst()
+        val left = rule.getLeft()
+        val right = rule.getRight()
+        for (a in left.get()) {
+            val newLeft = AttributesPack()
+            newLeft.add(left)
+            newLeft.remove(a)
+            val newRight = getClosure(newLeft, rulesPack)
+            if (newRight.contains(right)) {
+                val newRule = SimpleRule()
+                newRule.addLeft(newLeft)
+                newRule.addRight(right)
+                curr.addRule(newRule)
+                deq.addLast(newRule)
+                curr.removeRule(rule)
+            }
+        }
+    }
+
+    for (rule in curr.rules) {
+        val left = rule.getLeft()
+        val right = rule.getRight()
+        val currCopy = SimpleRulesPack()
+        currCopy.copy(curr)
+        currCopy.removeRule(rule)
+        val newRight = getClosure(left, currCopy)
+        if (newRight.contains(right)) {
+            curr = currCopy
+        }
+    }
+
     return curr
 }
 
@@ -183,8 +349,8 @@ fun main() {
         if (rule.size != 2) {
             throw RuntimeException("bad split by \"->\": $line")
         }
-        val ruleLeft = rule[0].split(" ")
-        val ruleRight = rule[1].split(" ")
+        val ruleLeft = rule[0].split(Regex("\\s|,"))
+        val ruleRight = rule[1].split(Regex("\\s|,"))
 
         val commonRule = CommonRule()
         for (x in ruleLeft) {
@@ -203,13 +369,15 @@ fun main() {
 
     println(rulesPack)
     println()
+    println(getIrreducible(rulesPack))
+    println()
     println("? enter the set of attributes you want to close:")
 
     val attributesPack = AttributesPack()
 
     line = readLine()
     while (line != null && line != "exit") {
-        val attributes: List<String> = line.split(" ")
+        val attributes: List<String> = line.split(Regex("\\s|,"))
         for (x in attributes) {
             if (x != "")
                 attributesPack.add(Attribute(x))
